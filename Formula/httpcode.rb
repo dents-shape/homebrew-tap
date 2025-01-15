439e1a33b7bb682af5c15d1f26ec030135490cc0
Faithfulness Alamu <42256651+faithfulnessalamu@users.noreply.github.com>
2025-01-16 00:46:07 +0100

    formula/httpcode: bump version and fix install method

--- Formula/httpcode.rb
+++ Formula/httpcode.rb
@@ -1,15 +1,15 @@
 class Httpcode < Formula
   desc "Get info about http status codes right at your terminal"
   homepage "https://github.com/faithfulnessalamu/httpcode"
-  url "https://github.com/faithfulnessalamu/httpcode/releases/download/v1.1/httpcode-mac-arm64.tar.gz"
+  url "https://github.com/faithfulnessalamu/httpcode/releases/download/v1.2/httpcode-mac-arm64.tar.gz"
   sha256 "b1a271e427d79f52bedbf2e6b7ae4b0a458d8afb15b20c39564705c29cec447d"
   license "MIT"
 
   def install
-    system "./configure", "--disable-silent-rules", *std_configure_args
+    bin.install "httpcode"
   end
 
   test do
-    system "false"
+    system "true"
   end
 end
