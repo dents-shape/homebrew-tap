39c586a0d0f50103dad4496c396a0980c896a4e1
Faithfulness Alamu <42256651+faithfulnessalamu@users.noreply.github.com>
2025-01-16 00:48:21 +0100

    formula/httpcode: fix broken sha

--- Formula/httpcode.rb
+++ Formula/httpcode.rb
@@ -2,7 +2,7 @@ class Httpcode < Formula
   desc "Get info about http status codes right at your terminal"
   homepage "https://github.com/faithfulnessalamu/httpcode"
   url "https://github.com/faithfulnessalamu/httpcode/releases/download/v1.2/httpcode-mac-arm64.tar.gz"
-  sha256 "b1a271e427d79f52bedbf2e6b7ae4b0a458d8afb15b20c39564705c29cec447d"
+  sha256 "3e6bc41a764ec6aeaf1140187c6f0f66e2ee3cb263bb78a96a2df9b9247e8be8"
   license "MIT"
 
   def install
